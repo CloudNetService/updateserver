2a91bb545810ba396ad90ca802f5fc8d34251b5e
derrop <derrop@outlook.de>
2019-12-17 16:57:49 +0100

    Initial commit

---
+++ src/main/java/eu/cloudnetservice/cloudnet/repository/CloudNetUpdateServer.java
@@ -0,0 +1,15 @@
+package eu.cloudnetservice.cloudnet.repository;
+
+public class CloudNetUpdateServer {
+
+
+
+    private CloudNetUpdateServer() {
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
