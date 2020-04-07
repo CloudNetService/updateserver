edf772523ea8cc313c0daa07ae4ed77ce1df760a
derrop <derrop@outlook.de>
2020-04-07 04:00:55 +0200

    Add api documentation to the update repository

--- src/main/java/eu/cloudnetservice/cloudnet/repository/repository/CloudNetUpdateRepository.java
+++ src/main/java/eu/cloudnetservice/cloudnet/repository/repository/CloudNetUpdateRepository.java
@@ -5,7 +5,10 @@ import eu.cloudnetservice.cloudnet.repository.database.Database;
 import eu.cloudnetservice.cloudnet.repository.version.CloudNetVersion;
 import eu.cloudnetservice.cloudnet.repository.version.CloudNetVersionFile;
 import io.javalin.Javalin;
+import io.javalin.http.Handler;
 import io.javalin.http.NotFoundResponse;
+import io.javalin.plugin.openapi.dsl.OpenApiUpdater;
+import io.swagger.v3.oas.models.Operation;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -13,8 +16,12 @@ import java.util.stream.Collectors;
 
 import static io.javalin.apibuilder.ApiBuilder.get;
 import static io.javalin.apibuilder.ApiBuilder.path;
+import static io.javalin.plugin.openapi.dsl.OpenApiBuilder.document;
+import static io.javalin.plugin.openapi.dsl.OpenApiBuilder.documented;
 
 public class CloudNetUpdateRepository {
+
+    private static final String WEB_API_DOCS_TAG = "Update Repository";
 
     private final String repositoryVersion = "1.0";
     private final String targetParentVersion;
@@ -50,28 +57,42 @@ public class CloudNetUpdateRepository {
 
     public void init(String pathPrefix, Javalin javalin) {
         javalin.routes(() -> path(pathPrefix, () -> {
-            get("/repository", ctx -> ctx.result(this.repositoryInformation));
-            get("/versions/:version/:file", ctx -> {
-                var version = this.database.getVersion(this.targetParentVersion, ctx.pathParam("version"));
-                if (version == null) {
-                    throw new NotFoundResponse("Version not found");
-                }
-                var fileName = ctx.pathParam("file");
-                Arrays.stream(version.getFiles())
-                        .filter(file -> file.getName().equals(fileName))
-                        .filter(file -> file.getFileType() != CloudNetVersionFile.FileType.JAVA_DOCS)
-                        .filter(file -> file.getFileType() != CloudNetVersionFile.FileType.CLOUDNET_ZIP)
-                        .findFirst()
-                        .ifPresentOrElse(file -> {
-                            try {
-                                ctx.result(this.releaseArchiver.openFileStream(version, file));
-                            } catch (IOException exception) {
-                                exception.printStackTrace();
-                            }
-                        }, () -> {
-                            throw new NotFoundResponse("File not found");
-                        });
-            });
+            get("/repository", documented(
+                    document()
+                            .operation((OpenApiUpdater<Operation>) operation -> operation
+                                    .summary("Get the repository version, cloudnet version (with git commit hash) and a list of all available files for this version")
+                                    .addTagsItem(WEB_API_DOCS_TAG)
+                            )
+                            .result("200", String.class, "text/x-java-properties"),
+                    (Handler) ctx -> ctx.result(this.repositoryInformation).contentType("text/x-java-properties")
+            ));
+            get("/versions/:version/:file", documented(
+                    document()
+                            .operation((OpenApiUpdater<Operation>) operation -> operation.summary("Download a file for a specific version").addTagsItem(WEB_API_DOCS_TAG))
+                            .result("200", null, "application/zip")
+                            .result("404", (Class<?>) null, apiResponse -> apiResponse.description("File or Version not found")),
+                    ctx -> {
+                        var version = this.database.getVersion(this.targetParentVersion, ctx.pathParam("version"));
+                        if (version == null) {
+                            throw new NotFoundResponse("Version not found");
+                        }
+                        var fileName = ctx.pathParam("file");
+                        Arrays.stream(version.getFiles())
+                                .filter(file -> file.getName().equals(fileName))
+                                .filter(file -> file.getFileType() != CloudNetVersionFile.FileType.JAVA_DOCS)
+                                .filter(file -> file.getFileType() != CloudNetVersionFile.FileType.CLOUDNET_ZIP)
+                                .findFirst()
+                                .ifPresentOrElse(file -> {
+                                    try {
+                                        ctx.result(this.releaseArchiver.openFileStream(version, file)).contentType("application/zip");
+                                    } catch (IOException exception) {
+                                        exception.printStackTrace();
+                                    }
+                                }, () -> {
+                                    throw new NotFoundResponse("File not found");
+                                });
+                    }
+            ));
         }));
     }
 
